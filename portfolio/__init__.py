5fc601cc5b0abd9fc6a376a28cff28bcb6de3606
leekempson <leekempson@hotmail.com>
2025-01-03 17:20:09 +0100

    initial commit

---
+++ portfolio/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask import send_from_directory
+
+
+app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
